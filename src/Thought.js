ebd948f108435b04e303d414b5da42cb4b2e63c6
shansach <mail.shantanusachdeva@gmail.com>
2022-11-16 23:43:06 -0500

    Disabling eslint

--- src/Thought.js
+++ src/Thought.js
@@ -6,11 +6,13 @@ export const Thought = (props)=>{
 
     useEffect(()=>{
         const timeLeft = thought.expiresAt - Date.now();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         const timout = setTimeout(()=>removeThought(thought), timeLeft);
         return ()=>clearTimeout(timout);
     }, [thought]);
 
     const handleRemove = ()=>{
+        
         removeThought(thought);
     }
     
